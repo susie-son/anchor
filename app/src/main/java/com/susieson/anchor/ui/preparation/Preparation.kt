d4c8461b35d03fb35534e2f10d04287bc52bd722
Susie Son <susieson99@gmail.com>
2024-05-25 01:14:14 -0400

    add alert dialogs

--- app/src/main/java/com/susieson/anchor/ui/preparation/Preparation.kt
+++ app/src/main/java/com/susieson/anchor/ui/preparation/Preparation.kt
@@ -7,16 +7,22 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -32,10 +38,12 @@ import com.susieson.anchor.ui.theme.AnchorTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun PreparationTopBar(modifier: Modifier = Modifier, onBack: () -> Unit) {
+fun PreparationTopBar(
+    modifier: Modifier = Modifier, onDiscard: () -> Unit = {}, onConfirm: () -> Unit = {}
+) {
     TopAppBar(title = { Text(text = stringResource(R.string.preparation_top_bar_title)) },
         navigationIcon = {
-            IconButton(onClick = onBack) {
+            IconButton(onClick = onDiscard) {
                 Icon(
                     painter = painterResource(id = R.drawable.icon_close),
                     contentDescription = "Close"
@@ -43,7 +51,7 @@ fun PreparationTopBar(modifier: Modifier = Modifier, onBack: () -> Unit) {
             }
         },
         actions = {
-            IconButton(onClick = { /*TODO*/ }) {
+            IconButton(onClick = onConfirm) {
                 Icon(
                     painter = painterResource(id = R.drawable.icon_done),
                     contentDescription = "Done"
@@ -62,10 +70,19 @@ fun Preparation(modifier: Modifier = Modifier, onBack: () -> Unit = {}) {
     var interpretations by remember { mutableStateOf(listOf<String>()) }
     var behaviors by remember { mutableStateOf(listOf<String>()) }
     var actions by remember { mutableStateOf(listOf<String>()) }
+    var openDiscardDialog by remember { mutableStateOf(false) }
+    var openConfirmDialog by remember { mutableStateOf(false) }
 
-    Scaffold(
-        modifier = modifier.fillMaxSize(),
-        topBar = { PreparationTopBar(onBack = onBack) }) { innerPadding ->
+    if (openDiscardDialog) {
+        DiscardDialog(onConfirm = onBack, onDismiss = { openDiscardDialog = false })
+    } else if (openConfirmDialog) {
+        ConfirmDialog(onConfirm = { /*TODO*/ }, onDismiss = { openConfirmDialog = false })
+    }
+
+    Scaffold(modifier = modifier.fillMaxSize(), topBar = {
+        PreparationTopBar(onDiscard = { openDiscardDialog = true },
+            onConfirm = { openConfirmDialog = true })
+    }) { innerPadding ->
         Column(
             modifier = modifier
                 .fillMaxSize()
@@ -145,10 +162,71 @@ fun Preparation(modifier: Modifier = Modifier, onBack: () -> Unit = {}) {
     }
 }
 
-@Preview
+@Composable
+fun DiscardDialog(onConfirm: () -> Unit = {}, onDismiss: () -> Unit = {}) {
+    AlertDialog(text = { Text(stringResource(R.string.preparation_discard_dialog_text)) },
+        onDismissRequest = onDismiss,
+        confirmButton = { TextButton(onClick = onConfirm) { Text(stringResource(R.string.preparation_discard_dialog_confirm)) } },
+        dismissButton = { TextButton(onClick = onDismiss) { Text(stringResource(R.string.preparation_discard_dialog_dismiss)) } })
+}
+
+@Composable
+fun ConfirmDialog(
+    modifier: Modifier = Modifier, onConfirm: () -> Unit = {}, onDismiss: () -> Unit = {}
+) {
+    val checked = remember { mutableStateListOf(false, false) }
+
+    AlertDialog(icon = {
+        Icon(
+            painter = painterResource(id = R.drawable.icon_anchor), contentDescription = null
+        )
+    },
+        title = { Text(stringResource(R.string.preparation_confirm_dialog_title)) },
+        text = {
+            Column(modifier = modifier) {
+                Text(
+                    stringResource(R.string.preparation_confirm_dialog_text),
+                    modifier = Modifier.padding(start = 16.dp, end = 16.dp, bottom = 8.dp)
+                )
+                ListItem(headlineContent = { Text(stringResource(R.string.preparation_confirm_dialog_check_1)) },
+                    trailingContent = {
+                        Checkbox(checked = checked[0], onCheckedChange = { checked[0] = it })
+                    })
+                HorizontalDivider()
+                ListItem(headlineContent = { Text(stringResource(R.string.preparation_confirm_dialog_check_2)) },
+                    trailingContent = {
+                        Checkbox(checked = checked[1], onCheckedChange = { checked[1] = it })
+                    })
+            }
+        },
+        onDismissRequest = onDismiss,
+        confirmButton = {
+            TextButton(onClick = onConfirm,
+                enabled = checked.all { it }) { Text(stringResource(R.string.preparation_confirm_dialog_confirm)) }
+        },
+        dismissButton = { TextButton(onClick = onDismiss) { Text(stringResource(R.string.preparation_confirm_dialog_dismiss)) } })
+}
+
+@Preview(showSystemUi = true)
 @Composable
 fun PreparationPreview() {
     AnchorTheme {
         Preparation()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DiscardDialogPreview() {
+    AnchorTheme {
+        DiscardDialog()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ConfirmDialogPreview() {
+    AnchorTheme {
+        ConfirmDialog()
     }
 }
