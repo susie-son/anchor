1ce8b2624c957c33e3b11ecf633b418b7a9dfc57
Susie Son <susieson99@gmail.com>
2024-06-23 06:57:37 -0400

    reorder imports

--- app/src/main/java/com/susieson/anchor/MainViewModel.kt
+++ app/src/main/java/com/susieson/anchor/MainViewModel.kt
@@ -5,10 +5,10 @@ import androidx.lifecycle.viewModelScope
 import com.susieson.anchor.model.AnchorUser
 import com.susieson.anchor.service.AuthService
 import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel
