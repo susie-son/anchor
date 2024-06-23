c8d9308eae953f9e6e905d1ac2166da842942ac3
Susie Son <susieson99@gmail.com>
2024-06-23 06:53:36 -0400

    add kotlin serialization plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("com.google.dagger.hilt.android")
     id("com.google.devtools.ksp")
     id("io.gitlab.arturbosch.detekt")
+    kotlin("plugin.serialization") version "2.0.0"
 }
 
 android {
