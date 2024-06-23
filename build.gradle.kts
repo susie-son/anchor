275ba9ed06e6b3e229c1a593a602fb1ad3a81771
Susie Son <susieson99@gmail.com>
2024-06-23 05:01:38 -0400

    Setup Detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.compose.compiler) apply false
-    id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
-    id("com.google.gms.google-services") version "4.4.1" apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
-    id("org.jlleitschuh.gradle.ktlint") version "12.1.1" apply false
-}
-
-subprojects {
-    apply(plugin = "org.jlleitschuh.gradle.ktlint")
-    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
-        debug.set(true)
-        android.set(true)
-    }
+    id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
+    id("io.gitlab.arturbosch.detekt") version "1.23.6" apply false
 }
